e9655619472023b5c249aea4fd1c02aecf66ca43
abhishekkaundal0052 <kaundal.abhishek0052@gmail.com>
2024-02-24 16:18:18 +0530

    Done

---
+++ script.js
@@ -0,0 +1,12 @@
+
+class ReactContainer extends React.Component{
+    
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll`)
+          );
+      }
+    
+  }
+  const container = document.getElementById('react-container');
+  ReactDOM.render(React.createElement(ReactContainer),container);
